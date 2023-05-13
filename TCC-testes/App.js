b2399d5c393d0f0d5e97b3246c9f1d1b01df675c
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-05-13 18:44:09 -0300

    Add files via upload

--- TCC-testes/App.js
+++ TCC-testes/App.js
@@ -1,23 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 
 const PlaceholderImage = require('./assets/background-image.png');
+;
 
-import Button from './componentes/button';
-import ImageViewer from './componentes/imageViewer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.imageConteiner}>
-        <ImageViewer placeholderImageSource={PlaceholderImage}></ImageViewer>
-      </View>
-      <Text style={styles.texto}>Omg, it's Madeline from Celeste</Text>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
+      <ImageBackground style= {styles.image_background} source={require('./assets/fundo_tela_login_cadastro1.png')}>
+        <Image
+          source={require('./assets/background-image.png')}
+          style={styles.logo}
+        />
+        <Text> oi </Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -25,31 +21,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
 
-  imageConteiner:{
-    flex: 1,
-    paddingTop: 58, 
-  },
+  logo: {
+    width: 150,
+    resizeMode: 'contain',
+    bottom: '30%',
+  },  
 
-  image:{
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-
-  texto:{
-    color: '#fff',
-    paddingBottom: 200,
-  },
-
-  footerContainer: {
-    flex: 1 / 3,
+  image_background: {
+    height: '100%',
+    width: '100%',
     alignItems: 'center',
-  },
+    justifyContent: 'center'
+  }
 
 });
-
