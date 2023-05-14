8ee1fd74d282b0f028d37af9c37ad59ab489b9b0
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-05-13 22:36:44 -0300

    Add files via upload

--- TCC-testes/App.js
+++ TCC-testes/App.js
@@ -1,42 +1,10 @@
+import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
-
-const PlaceholderImage = require('./assets/background-image.png');
-;
-
+import  Login  from './pages/login/login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground style= {styles.image_background} source={require('./assets/fundo_tela_login_cadastro1.png')}>
-        <Image
-          source={require('./assets/background-image.png')}
-          style={styles.logo}
-        />
-        <Text> oi </Text>
-      </ImageBackground>
-    </View>
+    <Login/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-  },
-
-  logo: {
-    width: 150,
-    resizeMode: 'contain',
-    bottom: '30%',
-  },  
-
-  image_background: {
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-
-});
